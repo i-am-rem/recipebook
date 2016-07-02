cf00d53ea40696a3abf0adea29d6cdac1c04bbc1
rheam <raymonjayfilosopo@gmail.com>
2016-07-02 08:35:13 -0700

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('cartCtrl', function($scope) {
+
+})
+   
+.controller('cloudCtrl', function($scope) {
+
+})
+      
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+ 
